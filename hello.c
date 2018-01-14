3ea5e47523a26f28ab9716b4f21ff406d21df99b
Rishabh <happysaxenaiscool@gmail.com>
2018-01-14 14:23:21 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int N,i;
+    printf("Enter the Number to print hello number times");
+    scanf("%d",&N);
+    for(i=1;i<=N;i++)
+        printf("Hello\n");
+}

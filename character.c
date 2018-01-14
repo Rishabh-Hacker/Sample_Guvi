8843036c13c039ddb9f18e2360b8416848ed9301
Rishabh <happysaxenaiscool@gmail.com>
2018-01-14 13:19:58 +0530

    Create character.c

---
+++ character.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    char N;
+    printf("Enter the Character");
+    scanf("%c",&N);
+    if(N=='a'||N=='e'||N=='i'||N=='o'||N=='u'||N=='A'||N=='E'||N=='I'||N=='O'||N=='U')
+        printf("Vowel");
+    else
+        printf("Consonant");
+}

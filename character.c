18d6ba9d697ee483a4c962ae62d593a46bc1aff5
Rishabh <happysaxenaiscool@gmail.com>
2018-01-14 13:31:40 +0530

    Update character.c

--- character.c
+++ character.c
@@ -4,8 +4,12 @@ int main()
     char N;
     printf("Enter the Character");
     scanf("%c",&N);
+    if(isalpha(N)){
     if(N=='a'||N=='e'||N=='i'||N=='o'||N=='u'||N=='A'||N=='E'||N=='I'||N=='O'||N=='U')
         printf("Vowel");
     else
         printf("Consonant");
+    }
+    else
+        printf("Invalid Input");
 }

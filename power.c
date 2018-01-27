ddd18cb673715270f0b737f2d5ba66df672dcbbb
Rishabh <happysaxenaiscool@gmail.com>
2018-01-27 19:28:00 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int N1,N2,c=0;
+    printf("Enter the Number and Power Number");
+    scanf("%d%d",&N1,&N2);
+    c=pow(N1,N2);
+    printf("%d",c);
+    }

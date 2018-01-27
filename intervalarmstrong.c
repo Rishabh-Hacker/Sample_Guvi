915c7239450e875b6d2f5de136160c464b1ac854
Rishabh <happysaxenaiscool@gmail.com>
2018-01-27 23:41:12 +0530

    Create intervalarmstrong.c

---
+++ intervalarmstrong.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int N,Q,i,j;
+    printf("Enter the Two Numbers");
+    scanf("%d%d",&N,&Q);
+    if(N<Q){
+        i=N+1;
+        j=Q;
+    }
+    else{
+        i=Q+1;
+        j=N;
+    }
+    for(;i<j;i++){
+        armstrong(i);
+    }
+}
+int armstrong(int N){
+int c,i,h,x=0,s=0,j=0;
+h=N;
+c=N;
+while(c!=0){
+    c=c/10;
+    j+=1;
+    }
+while(N!=0){
+    x=N%10;
+    s=s+pow(x,j);
+    N=N/10;
+    }
+if(h==s)
+printf("%d",s);
+}

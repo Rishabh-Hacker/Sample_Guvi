297a06f620b2210590889f7288b0bded7382804b
Rishabh <happysaxenaiscool@gmail.com>
2018-02-05 23:26:08 +0530

    Create fact_range.c

---
+++ fact_range.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    long long int N,i=1,j;
+    printf("Enter the number\n");
+    scanf("%d",&N);
+    if(N<=20){
+    for(j=N;j>=1;j--)
+    i=i*j;
+    printf("%lld",i);
+    }
+    else{
+    printf("Out of range");
+    }
+}
